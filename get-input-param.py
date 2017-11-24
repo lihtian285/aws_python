0295d77ac508423cc1eb92a0a7160fb472d4d2fb
Lih Tian <lihtian@gmail.com>
2017-11-24 15:28:14 +0800

    Update get-input-param.py

--- get-input-param.py
+++ get-input-param.py
@@ -1,3 +1,8 @@
 import sys
 print sys.argv[0]  #this will print the script name
 print sys.argv[1]  #this will print the param input name
+
+#Sample output
+#$ python get-input.py /home/linuxadmin
+#get-input.py
+#/home/linuxadmin
